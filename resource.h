3dc05d6c239caf1c0d7316243ff3c7fab5355d1b
steelm <steelm@madcat.cc>
2024-07-03 18:53:25 +0800

    change about box

--- resource.h
+++ resource.h
@@ -26,14 +26,16 @@
 #define IDC_PROGRESS_TRANSFER           1006
 #define IDC_LIST_ERROR                  1008
 #define IDC_BUTTON_SRC_FILE             1009
+#define IDC_MFCLINK1                    1010
+#define IDC_MFCLINK_HOME                1010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
