f8124e4bd6c108fe373181db2e431b50ce5f42a9
steelm <steelm@madcat.cc>
2024-07-03 12:53:06 +0800

    build env fix

--- pack.bat
+++ pack.bat
@@ -3,6 +3,6 @@ mkdir Release\zip\x86
 mkdir Release\zip\x64
 
 copy x64\Release\CBF.exe Release\zip\x64
-copy x86\Release\CBF.exe Release\zip\x86
+copy Win32\Release\CBF.exe Release\zip\x86
 
 Tools\7-Zip\7z.exe a latest.zip Release\zip\*
